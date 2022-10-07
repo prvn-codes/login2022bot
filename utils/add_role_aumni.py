1ce8e69ec919348054bfa06fc215253720c13399
Praveen S <praveensampath2706@gmail.com>
2022-10-07 23:31:38 +0530

    added alumni

--- utils/add_role_aumni.py
+++ utils/add_role_aumni.py
@@ -10,7 +10,7 @@ async def addRoleAlumni(bot, message: discord.message.Message, conn: db):
     
     if userRegistered != "UserNameNotFound":
         if guild.id == int(os.environ['LAST_STAND_GUILD']):
-            user.send("Welcome Alumni")
+            await user.send("Welcome Alumni")
             return
         await user.edit(nick=userRegistered.title())
         await user.edit(roles=[])
