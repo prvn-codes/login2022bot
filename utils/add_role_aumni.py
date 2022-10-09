d0bc4c598a02a4f7b5d523bbde30ea1e0cf3a5b6
Praveen S <praveensampath2706@gmail.com>
2022-10-09 21:50:03 +0530

    updated alumni auto response

--- utils/add_role_aumni.py
+++ utils/add_role_aumni.py
@@ -10,7 +10,7 @@ async def addRoleAlumni(bot, message: discord.message.Message, conn: db):
     
     if userRegistered != "UserNameNotFound":
         if guild.id == int(os.environ['LAST_STAND_GUILD']):
-            await user.send("As an esteemed alumni of our departments, we welcome you, whose constant source of encouragement and support has been the backbone of our growth over the years. It must be noted that our departments have always emphasized on the bond between the senior and junior batches on campus, and that this extends into all kinds of support, mentoring and avenues for learning even after graduation.\n\nPlease Join Login'22 discord server too. https://discord.com/invite/RTrVjqMYF8")
+            await user.send(f"As an esteemed alumni of our departments, we welcome you {userRegistered.title()}, whose constant source of encouragement and support has been the backbone of our growth over the years. It must be noted that our departments have always emphasized on the bond between the senior and junior batches on campus, and that this extends into all kinds of support, mentoring and avenues for learning even after graduation.\n\nPlease Join Login'22 discord server too. https://discord.com/invite/RTrVjqMYF8")
             return
         await user.edit(nick=userRegistered.title())
         await user.edit(roles=[])
