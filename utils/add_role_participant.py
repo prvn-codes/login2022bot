de161aa32670d49fa6479c9ca94ffba519df9800
Praveen S <praveensampath2706@gmail.com>
2022-10-10 22:40:20 +0530

    stopped dming people.

--- utils/add_role_participant.py
+++ utils/add_role_participant.py
@@ -45,7 +45,8 @@ async def addRoleParticipant(bot, message: discord.message.Message, log, conn):
             roles.append(discord.utils.get(guild.roles, id=eventRoleMapping["ls-nfs"]))
 
         else:
-          await user.send("Thank you for registering! However, we notice that you have not registered for Last Stand event. Please register for any event of your choice! You will have to submit your registered email ID again for this purpose. If you have any queries with regard to this, feel free to reach out to the <#1020390088200437760> channel or <#1025079500343607346> by tagging registration team or the server admin.\n\n Website : https://www.psglogin.in\n\nIf you have registered for other events from Login'22 please join our **Login'22 Discord Server** at https://discord.com/invite/RTrVjqMYF8")
+          # await user.send("Thank you for registering! However, we notice that you have not registered for Last Stand event. Please register for any event of your choice! You will have to submit your registered email ID again for this purpose. If you have any queries with regard to this, feel free to reach out to the <#1020390088200437760> channel or <#1025079500343607346> by tagging registration team or the server admin.\n\n Website : https://www.psglogin.in\n\nIf you have registered for other events from Login'22 please join our **Login'22 Discord Server** at https://discord.com/invite/RTrVjqMYF8")
+          pass
         
         nickname = db.get_user_name(message.content, conn).title()
         await user.edit(nick=nickname)
@@ -71,18 +72,18 @@ async def addRoleParticipant(bot, message: discord.message.Message, log, conn):
       msg = f"Thank you for registering!\nHere is the list of Events you have registered : \n{role_names}\nIf your registered events are missing or misassigned please feel free to contact server admin or mod"
       if message.guild.id == int(os.environ['LAST_STAND_GUILD']):
         msg = f"Thank you for registering!\nHere is the list of Events you have registered from Last Stand : \n{role_names}\nIf your registered events are missing or misassigned please feel free to contact server admin or mod"
-      await user.send(msg)
+      # await user.send(msg)
       log.write(f"[{datetime.now(pytz.timezone('Asia/Calcutta'))}] : [{message.guild.name}] [{user.name}] \t{message.content} added roles {roles}\n")
     
     else:
       log.write(f"- [{datetime.now(pytz.timezone('Asia/Calcutta'))}] : [{message.guild.name}] [{user.name}] \t`{message.content}` has not registered any events\n")
-      await user.send("Thank you for registering! However, we notice that you have not registered for any specific event. Please register for any event of your choice! You will have to submit your registered email ID again for this purpose. If you have any queries with regard to this, feel free to reach out to the <#1020388821742927932> channel or <#1025079557117710436> by tagging registration team or the server admin.\n\n Website : https://www.psglogin.in")
+      # await user.send("Thank you for registering! However, we notice that you have not registered for any specific event. Please register for any event of your choice! You will have to submit your registered email ID again for this purpose. If you have any queries with regard to this, feel free to reach out to the <#1020388821742927932> channel or <#1025079557117710436> by tagging registration team or the server admin.\n\n Website : https://www.psglogin.in")
   
   else:
     userRegistered = db.get_alumni(message.content, conn)
     if userRegistered == "UserNameNotFound":
       log.write(f"- [{datetime.now(pytz.timezone('Asia/Calcutta'))}] : [{message.guild.name}] [{user.name}] \t`{message.content}` is Not A Registered Participant\n")
-      await user.send("Oh ooh! We notice that you have not registered Login'22. Please register for any event of your choice! You will have to submit your email ID again for this purpose. If you have any queries with regard to this, feel free to reach out to the <#1020388821742927932> channel or <#1025079557117710436> or the server admin.\n\n Website : https://www.psglogin.in")
+      # await user.send("Oh ooh! We notice that you have not registered Login'22. Please register for any event of your choice! You will have to submit your email ID again for this purpose. If you have any queries with regard to this, feel free to reach out to the <#1020388821742927932> channel or <#1025079557117710436> or the server admin.\n\n Website : https://www.psglogin.in")
       return 0
     else:
       return 1
